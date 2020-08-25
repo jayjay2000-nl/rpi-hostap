4a268c23fbad00768c20c4293322aec18ea971df
jayjay2000-nl <57066191+jayjay2000-nl@users.noreply.github.com>
2020-08-26 01:59:13 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt install iw
+iw reg set NL
+sudo systemctl mask wpa_supplicant.service
+sudo mv /sbin/wpa_supplicant /sbin/no_wpa_supplicant
+sudo pkill wpa_supplicant
+
+ifconfig wlan0 192.168.254.1/24 up
+sudo docker run -d -t \
+  -e INTERFACE=wlan0 \
+  -e CHANNEL=6 \
+  -e SSID=IOT \
+  -e AP_ADDR=192.168.254.1 \
+  -e SUBNET=192.168.254.0 \
+  -e WPA_PASSPHRASE=adminIOT \
+  -e OUTGOINGS=eth0 \
+  --privileged \
+  --net host \
+  sdelrio/rpi-hostap:latest
